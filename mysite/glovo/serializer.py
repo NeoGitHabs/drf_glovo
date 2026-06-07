edd1d915a0759a1d96d0faa61be5e1bc774d04b1
Neo <mrrneo@outlook.com>
2026-06-07 07:30:11 +0600

    ...

--- mysite/glovo/serializer.py
+++ mysite/glovo/serializer.py
@@ -8,37 +8,44 @@ class UserProfileSerializers(serializers.ModelSerializer):
         model = UserProfile
         fields = ('first_name', 'last_name', 'status')
 
+
 class CategorySerializers(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ('category_name',)
+
 
 class ContactSerializers(serializers.ModelSerializer):
     class Meta:
         model = Contact
         fields = ('contact_name', 'phone_number', 'social_network')
 
+
 class ProductSerializers(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ('product_name', 'product_image', 'product_description', 'product_price')
+
 
 class ComboSerializers(serializers.ModelSerializer):
     class Meta:
         model = Combo
         fields = ('combo_name', 'combo_image', 'combo_description', 'combo_price')
 
-class UserNickMameForReview(serializers.ModelSerializer):
+
+class UserNickNameForReview(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
         fields = ('username',)
 
+
 class StoreReviewSerializers(serializers.ModelSerializer):
-    client_profile = UserNickMameForReview()
-    created_date = serializers.DateTimeField(format('%d-%m-%y %H-%M'))
+    client_profile = UserNickNameForReview()
+    created_date = serializers.DateTimeField(format='%d-%m-%y %H:%M')
     class Meta:
         model = StoreReview
         fields = ('client_profile', 'review_text_store', 'stars_store', 'created_date')
+
 
 class StoreSerializers(serializers.ModelSerializer):
     owner_store = UserProfileSerializers()
@@ -47,25 +54,30 @@ class StoreSerializers(serializers.ModelSerializer):
     product_list = ProductSerializers(many=True, read_only=True)
     combo_list = ComboSerializers(many=True, read_only=True)
     store_review = StoreReviewSerializers(many=True, read_only=True)
+
     class Meta:
         model = Store
         fields = ('store_name', 'store_image', 'category', 'store_description', 'address',
                   'contacts', 'owner_store', 'product_list', 'combo_list', 'store_review')
+
 
 class CartSerializers(serializers.ModelSerializer):
     class Meta:
         model = Cart
         fields = ('cart_owner',)
 
+
 class OrderSerializers(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = ('order_status', 'delivery_address', 'courier_profile', 'order_time')
 
+
 class CourierSerializers(serializers.ModelSerializer):
     class Meta:
         model = Courier
         fields = ('courier_status',)
+
 
 class CourierRatingSerializers(serializers.ModelSerializer):
     class Meta:
--- mysite/glovo/views.py
+++ mysite/glovo/views.py
@@ -1,9 +1,7 @@
-from rest_framework import viewsets, generics
+from rest_framework import viewsets, generics, permissions
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
 from .filters import StoreFilter
-from rest_framework import permissions
-from .permissions import *
 from .models import (UserProfile, Category, Store, Contact, Product, Combo,
                      Cart, Order, Courier, StoreReview, CourierRating)
 from .serializer import (UserProfileSerializers, CategorySerializers, StoreSerializers, ContactSerializers,
@@ -14,77 +12,80 @@ from .serializer import (UserProfileSerializers, CategorySerializers, StoreSeria
 class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializers
-
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         return UserProfile.objects.filter(id=self.request.user.id)
 
+
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializers
+
 
 class StoreListAPIView(generics.ListAPIView):
     queryset = Store.objects.all()
     serializer_class = StoreSerializers
     filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
     filterset_class = StoreFilter
-    search_fields = ['store_name', 'product_name']
-    ordering_fields = ['product_price', 'combo_price']
-    ordering = ['product_price']
+    search_fields = ['store_name', 'store_description']
+    ordering_fields = ['store_name']
+    ordering = ['store_name']
+
 
 class ContactViewSet(viewsets.ModelViewSet):
     queryset = Contact.objects.all()
     serializer_class = ContactSerializers
 
+
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializers
 
+
 class ComboViewSet(viewsets.ModelViewSet):
     queryset = Combo.objects.all()
     serializer_class = ComboSerializers
-
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        return UserProfile.objects.filter(id=self.request.user.id)
+        return Combo.objects.filter(store__owner_store=self.request.user)
+
 
 class CartViewSet(viewsets.ModelViewSet):
     queryset = Cart.objects.all()
     serializer_class = CartSerializers
-
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        return UserProfile.objects.filter(id=self.request.user.id)
+        return Cart.objects.filter(cart_owner=self.request.user)
+
 
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializers
-
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        return UserProfile.objects.filter(id=self.request.user.id)
+        return Order.objects.filter(client_profile=self.request.user)
+
 
 class CourierViewSet(viewsets.ModelViewSet):
     queryset = Courier.objects.all()
     serializer_class = CourierSerializers
-
     permission_classes = [permissions.IsAuthenticated]
+
 
 class StoreReviewViewSet(viewsets.ModelViewSet):
     queryset = StoreReview.objects.all()
     serializer_class = StoreReviewSerializers
-
     permission_classes = [permissions.IsAuthenticated]
+
 
 class CourierRatingViewSet(viewsets.ModelViewSet):
     queryset = CourierRating.objects.all()
     serializer_class = CourierRatingSerializers
-
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        return UserProfile.objects.filter(id=self.request.user.id)
+        return CourierRating.objects.filter(client_profile=self.request.user)
